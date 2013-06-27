2cdb0379f54d5bdf874c1a4cca3dfb12d6908ff5
Julian Haslinger <haslingerjulian@aol.com>
2013-06-27 19:29:09 +0200

    first

---
+++ lightContentBox.jquery.js
@@ -0,0 +1,10 @@
+(function($){
+    $.gw2tooltip = function(selector, settings){
+        if(settings){
+            $.extend(true, config, settings);
+        }
+        console.log(this);
+        //vars
+        return this;
+    }
+});
