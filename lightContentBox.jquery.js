4852e83625d43cdb97f495e53357b191f2140beb
Julian Haslinger <haslingerjulian@aol.com>
2013-06-28 16:42:17 +0200

    update

--- lightContentBox.jquery.js
+++ lightContentBox.jquery.js
@@ -1,10 +1,61 @@
 (function($){
-    $.gw2tooltip = function(selector, settings){
-        if(settings){
-            $.extend(true, config, settings);
+    var contentlightbox = {
+        config: {
+            showDuration: 100,
+            hideDuration: 100,
+            contentBoxId: 'contentlightbox',
+            contentInner: '',
+            closeCallback: function() {
+            },
+            openCallback: function() {
+            }
+        },
+        $contentlightbox: function() {
+            return $(document.createElement('div')).hide().attr({id:contentlightbox.config.contentBoxId}).html([
+                $(document.createElement('div')).addClass('content').text('box'),
+                $(document.createElement('div')).addClass('close').text('close').click(function() {
+                    $('#contentlightbox').fadeOut(contentlightbox.config.hideDuration,function() {
+                        contentlightbox.config.closeCallback();
+                    });
+                })
+            ])
+        },
+        boxCenter: function() {
+            var $contentlightbox = $('#'+contentlightbox.config.contentBoxId);
+            $contentlightbox.css({
+                position:'fixed',
+                top:'48%',
+                left:'50%',
+                backgroundColor: 'red',
+                marginLeft:-$contentlightbox.width()/2,
+                marginTop:-$contentlightbox.height()/2
+            });
         }
-        console.log(this);
-        //vars
+    };
+    $.contentlightbox = function(selector, settings){
+        if(settings){
+            $.extend(true, contentlightbox.config, settings);
+        }
+
+        var $selector = $(selector);
+        var $box = contentlightbox.$contentlightbox();
+        $('body').append($box);
+
+        $selector.click(function() {
+            switch (contentlightbox.config.mode) {
+                case 'frame':
+                    $box.find('.content').html(contentlightbox.config.content);
+                    break;
+            }
+
+            contentlightbox.boxCenter();
+            $box.fadeIn(contentlightbox.config.showDuration, function() {
+                contentlightbox.config.openCallback();
+            })
+        });
+        $(window).resize(function() {
+            contentlightbox.boxCenter();
+        });
         return this;
     }
-});
+})(jQuery);
